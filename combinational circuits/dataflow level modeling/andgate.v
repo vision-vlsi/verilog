a56561135f5dea8b4a48e41aef9ca66f575a19cf
vision-vlsi <89384474+vision-vlsi@users.noreply.github.com>
2021-08-27 07:59:22 +0530

    Delete combinational circuits directory

--- combinational circuits/dataflow level modeling/andgate.v
+++
@@ -1,5 +0,0 @@
-module andgate(a,b,y);
-input a,b;
-output y;
-  assign y=a&b;  //and(y,a,b)
-endmodule
--- combinational circuits/gatelevel modeling/half_adder.v
+++
@@ -1,6 +0,0 @@
-module half_adder(a,b,sum,co);
-  input a,b;
-  output sum,co;
-  xor(sum,a,b);
-  and(co,a,b);
-endmodule
