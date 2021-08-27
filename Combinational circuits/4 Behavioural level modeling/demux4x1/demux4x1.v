bd736bcde88df2b4754c9327f517b7c3d51b7ab5
vision-vlsi <89384474+vision-vlsi@users.noreply.github.com>
2021-08-27 21:31:43 +0530

    Update demux4x1.v

--- Combinational circuits/4 Behavioural level modeling/demux4x1/demux4x1.v
+++ Combinational circuits/4 Behavioural level modeling/demux4x1/demux4x1.v
@@ -1,3 +1,5 @@
+//Faulty code
+
 module demux4x1(d,s1,s0,y0,y1,y2,y3);
   input d,s1,s0;
   output y0,y1,y2,y3;
@@ -8,4 +10,17 @@ module demux4x1(d,s1,s0,y0,y1,y2,y3);
   2'b10: y2=d;
   2'b11: y3=d;
  endcase
+endmodule
+
+//Good code
+module demux4x1(d,s1,s0,y0,y1,y2,y3);
+  input d,s1,s0;
+  output reg y0,y1,y2,y3;
+  always@*
+  case({s1,s0})
+  2'b00: begin y0=d; y1=1'b0; y2=1'b0; y3=1'b0; end
+  2'b01: begin y0=1'b0; y1=d; y2=1'b0; y3=1'b0; end
+  2'b10: begin y0=1'b0; y1=1'b0; y2=d; y3=1'b0; end
+  2'b11: begin y0=1'b0; y1=1'b0; y2=1'b0; y3=d; end
+  endcase
 endmodule
