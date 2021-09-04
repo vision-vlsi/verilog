0cb3059417e817454aebd71242c316f9e9cb6f03
vision-vlsi <89384474+vision-vlsi@users.noreply.github.com>
2021-09-04 06:52:18 +0530

    Delete Combinational circuits directory

--- Combinational circuits/3 Dataflow level modeling/mux2x1/mux2x1.v
+++
@@ -1,9 +0,0 @@
-module mux2x1(a,b,s,y);
-  input a,b,s;
-  output y;
-  wire sbar,o1,o2;
-   assign sbar=~s;    // not(sbar,s);
-   assign o1=sbar&a;  // and(o1,sbar,a);
-   assign o2=s&b;    //  and(o2,s,b);
-   assign y=o1|o2;  //  or(y,o1,o2);
-endmodule
--- Combinational circuits/4 Behavioural level modeling/andgate/andgate.v
+++
@@ -1,6 +0,0 @@
-module andgate(a,b,y);
-input a,b;
-output reg y;
-always@*
-  y=a&b;
-endmodule
